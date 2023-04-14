73304378203d42f4257efcf26463d34f3a605f14
NGUYENEM\T450s <nguyenvanem2k3@gmail.com>
2023-04-14 20:33:18 +0700

    Final Ass Code C

--- demoass.cpp
+++ demoass.cpp
@@ -1,6 +1,5 @@
-// Các thu vi?n c?n thi?t cho chuong trình
+// Các thu vien can thiet cho chuong trình
 #include <stdio.h>
-#include <conio.h>
 #include <math.h>
 #include <string.h>
 #include <stdlib.h>
@@ -44,17 +43,17 @@ void nhapngaythang(date &ntn)
     } while (ntn.ngay < 0 || ntn.ngay > 31);
     do
     {
-        printf("\n\tNhap vao thang:");
+        printf("\tNhap vao thang:");
         scanf("%d", &ntn.thang);
     } while (ntn.thang < 0 || ntn.thang > 12);
-    printf("\n\tNhap vao nam:");
+    printf("\tNhap vao nam:");
     scanf("%d", &ntn.nam);
 }
 void nhap1nv(nhanvien &nv)
 {
     printf("\tNhap vao msnv:");
     scanf("%d", &nv.manv);
-    printf("\n\tNhap vao ho:");
+    printf("\tNhap vao ho:");
     fflush(stdin);
     gets(nv.ho);
     printf("\tNhap vao ten:");
@@ -75,46 +74,62 @@ void nhap1nv(nhanvien &nv)
 }
 void xuatngaythang(date ntn)
 {
-    printf("\t%d-%d-%d", ntn.ngay, ntn.thang, ntn.nam);
+    printf("\n\t%d-%d-%d", ntn.ngay, ntn.thang, ntn.nam);
 }
 void xuat1nv(nhanvien nv)
 {
-	printf("\tThong tinh nhan vien!");
-    printf("\tMa so nhan vien:%d", nv.manv);
-    printf("\tHo:%s", nv.ho);
-    printf("\tTen:%s", nv.ten);
-    printf("\tNgay thang nam sinh nhan vien:");
+	printf("\n\tThong tinh nhan vien!");
+    printf("\n\tMa so nhan vien:%d", nv.manv);
+    printf("\n\tHo:%s", nv.ho);
+    printf("\n\tTen:%s", nv.ten);
+    printf("\n\tNgay thang nam sinh nhan vien:");
     xuatngaythang(nv.namsinh);
-    printf("\tNoi sinh:%s", nv.noisinh);
-    printf("\tDia chi:%s", nv.diachi);
-    printf("\tLuong:%f", nv.luong);
-    printf("\tNgay vao:");
+    printf("\n\tNoi sinh:%s", nv.noisinh);
+    printf("\n\tDia chi:%s", nv.diachi);
+    printf("\n\tLuong:%f", nv.luong);
+    printf("\n\tNgay vao:");
     xuatngaythang(nv.ngayvao);
 }
 
 void nhapdanhsach(danhsach &ds)
 {
-    printf("\tNhap so luong nhan vien:");
-    scanf("%d", &ds.n);
-    printf("\tNhap thong tinh nhan vien:\n");
+    do{
+    	printf("\tNhap so luong nhan vien:");
+    	scanf("%d", &ds.n);
+	}while(ds.n<=0);
+    printf("\tMoi nhap thong tinh nhan vien!");
     for (int i = 0; i < ds.n; i++)
     {
         printf("\n\tNhan vien thu %d\n", i + 1);
         nhap1nv(ds.a[i]);
-        printf("===================================\n");
+        printf("================================================================\n");
     }
 }
 void xuatdanhsach(danhsach ds)
 {
-    printf("\tDanh sach nhan vien:");
+    printf("\tDanh sach nhan vien:\n");
     for (int i = 0; i < ds.n; i++)
     {
         printf("\tNhan vien thu %d", i + 1);
         xuat1nv(ds.a[i]);
-        printf("\n===================================\n");
+        printf("\n================================================================\n");
     }
 }
-// int timten(danhsach ds, char *c);
+
+void timten(danhsach ds, char *c){
+	int i;
+	int dem=0;
+ 	for (int i = 0; i < ds.n; i++){
+ 		if(strcmp(strlwr(ds.a[i].ten),strlwr(c))==0){
+ 			printf("\n\tDa tim thay nhan vien o vi tri:%d",i+1);
+ 			xuat1nv(ds.a[i]);
+ 			dem++;
+ 		}
+	} 
+	if(dem==0){
+		printf("\n\tKhong tim thay nhan vien");
+	}
+}
 void themnv(danhsach &ds, nhanvien nv, int k)
 {
     for (int i = ds.n; i > k; i--)
@@ -134,7 +149,31 @@ int timMSNV(danhsach ds, int d){
 	}
 	return -1;
 }
-// void xoanv(danhsach ds, int d);
+void sapxep(danhsach &ds){
+	  int i, j;
+    for (i = 0; i < ds.n - 1; i++) {
+        for (j = i + 1; j < ds.n; j++) {
+            if (ds.a[i].luong < ds.a[j].luong) {
+                nhanvien temp = ds.a[i];
+                ds.a[i] = ds.a[j];
+                ds.a[j] = temp;
+            }
+        }
+    }	
+}
+void xoanv(danhsach *ds, int d){
+	int vtri = timMSNV(*ds,d);
+	int i;
+	if(vtri==-1){
+		printf("\n\tKhong tim thay nhan vien de xoa");
+	}else{
+		for(i=vtri;i<ds->n-1;i++){
+			ds->a[i] = ds->a[i+1];
+		}
+		printf("\n\tDa xoa nhan vien tai vi tri:%d",vtri+1);
+		ds->n--;
+	}
+}
 
 void menu()
 {
@@ -171,10 +210,10 @@ void menu()
         case 2:
             printf("\n\t 2.Tim nhan vien theo ma so");
             printf("\n\t--------------------------------");
-            printf("\n\t Moi nhap ma so sinh vien can tim:");
+            printf("\n\t Moi nhap ma so nhan vien can tim:");
             scanf("%d",&d);
             if(timMSNV(ds,d)!=-1){
-            	printf("\n\tDa tim thay sinh vien o vi tri:%d",timMSNV(ds,d)+1);
+            	printf("\n\tDa tim thay nhan vien o vi tri:%d",timMSNV(ds,d)+1);
             	xuat1nv(ds.a[timMSNV(ds,d)]);
 			}else{
 				printf("\n\tKhong tim thay nhan vien");
@@ -182,12 +221,25 @@ void menu()
             break;
         case 3:
             printf("\n\t 3.Tim nhan vien theo ten");
+            char c[30];
+            printf("\n\t Moi nhap ten nhan vien can tim:");
+            fflush(stdin);
+    		gets(c);
+    		timten(ds,c);
             break;
         case 4:
-            printf("\n\t 4.Bang luong nhan vien giam dan");
+            printf("\n\t 4.Bang luong nhan vien giam dan\n");
+            sapxep(ds);
+            xuatdanhsach(ds);
             break;
         case 5:
             printf("\n\t 5.Xoa 1 nhan vien");
+            printf("\n\t--------------------------------");
+            printf("\n\t Moi nhap ma so nhan vien can xoa:");
+            scanf("%d",&d);
+            xoanv(&ds,d);
+        	printf("\n\tDanh sach nhan vien sau khi xoa!\n");
+            xuatdanhsach(ds);
             break;
         case 0:
             printf("\n\t Ban chon thoat");
@@ -205,7 +257,7 @@ void menu()
 int main()
 {
     menu();
-    getch();
+//	getch();//doc 1 ky tu tu ban phim khong can an enter
     return 0;
 }
 // =================================
